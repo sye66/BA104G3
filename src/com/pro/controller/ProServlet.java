9e61aa7d35242a5c4e7e5486c9a366087489b653
love2000evol <love2000evol@yahoo.com.tw>
2017-11-19 23:19:44 +0800

    檔案更新

--- src/com/pro/controller/ProServlet.java
+++ src/com/pro/controller/ProServlet.java
@@ -44,7 +44,7 @@ public class ProServlet extends HttpServlet {
 				 * 1.接收請求參數 - 輸入格式的錯誤處理
 				 **********************/
 				String pro_No = req.getParameter("pro_No");
-				
+System.out.println("前台進入商品編號: "+pro_No);				
 				if (pro_No == null || (pro_No.trim()).length() == 0) {
 					errorMsgs.add("輸入格式的錯誤");
 				}
