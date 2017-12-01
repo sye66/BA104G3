e7327740a17c128b885f65611585751c554603f2
Wei <sye663248@gmail.com>
2017-12-01 20:17:45 +0800

    update android mission model controller again

--- src/com/android/model/MissionDAO.java
+++ src/com/android/model/MissionDAO.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
-
+ 
 
 public class MissionDAO implements MissionDAO_interface {
 	String driver = "oracle.jdbc.driver.OracleDriver";
--- src/com/android/servlet/MissionServlet.java
+++ src/com/android/servlet/MissionServlet.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.LinkedList;
 import java.util.List;
-
+ 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
